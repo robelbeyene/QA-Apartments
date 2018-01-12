3e8a6dc4607c099d31eb43485643f42273594baa
chapmanoo <chapmanoo@hotmail.co.uk>
2018-01-12 15:47:21 +0000

    Added a test case to PersonDBImpleTest, needs work/clarification as understanding of testing an EE application and database is lacking

--- src/test/java/com/qa/apartment/business/PersonDBImpleTest.java
+++ src/test/java/com/qa/apartment/business/PersonDBImpleTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.qa.apartment.persistance.Person;
+
 public class PersonDBImpleTest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void createPersonFromStringTest() {
+		Person person1 = new Person(1, "Harry", "Potter", "harry.potter@qa.com", "01234567891");
+		PersonDBImple em = new PersonDBImple();
+		
+		assertEquals("Result not expected", em.findAllPersons().size(), 1);
+		
+		em.createPersonFromPerson(person1);
+		
+		assertEquals("Result not expected", em.findAllPersons().size(), 2);
 	}
 
 }
