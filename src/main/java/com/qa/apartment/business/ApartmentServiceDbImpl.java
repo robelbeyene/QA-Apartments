50976995728cb20b81ffa7150b7f6a658028fe90
dasp24 <daniel.parkes901@gmail.com>
2018-01-15 16:50:58 +0000

    added service implementation methods for apartment

--- src/main/java/com/qa/apartment/business/ApartmentServiceDbImpl.java
+++ src/main/java/com/qa/apartment/business/ApartmentServiceDbImpl.java
@@ -1,15 +1,48 @@
 package com.qa.apartment.business;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
+import com.qa.apartment.persistance.Apartment;
 import com.qa.apartment.util.JSONUtil;
 
+@Transactional(Transactional.TxType.SUPPORTS)
 public class ApartmentServiceDbImpl {
 	
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
-	private JSONUtil jsonUtil = new JSONUtil();
+//	private JSONUtil jsonUtil = new JSONUtil();
+	
+	public Apartment findApartment(long id) {
+		return em.find(Apartment.class,id);
+	}
+	
+	public List<Apartment> findAllApartments() {
+		TypedQuery<Apartment> query = em.createQuery("SELECT a FROM Apartment a ORDER BY a.id", Apartment.class);
+		return query.getResultList();
+	}
+	
+	@Transactional(Transactional.TxType.REQUIRED)
+	public String createApartment(Apartment apartment) {
+		em.persist(apartment);
+		return "added apartment";
+	}
+	
+	@Transactional(Transactional.TxType.REQUIRED)
+	public String deleteApartment(long id) {
+		em.remove(id);
+		return "Back you go, back into hell";
+	}
+	
+	@Transactional(Transactional.TxType.REQUIRED)
+	public String updateApartment(Apartment originalApartment,Apartment newApartment) {
+		newApartment = em.merge(originalApartment);
+		return "movie sucessfully updated";
+	}
 	
 	
 
