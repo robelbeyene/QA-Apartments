44c2f4163672cf40c5c6ddde3d5af30bb6519f14
Alasderp <AlasdairStuart@live.co.uk>
2018-01-15 10:57:20 +0000

    Delete import.sql

--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Apartment VALUES (1, 'The Heart', '12', 'LettingAgency', 'Leo Baker', 'QA', '01/01/2017', '12/12/17', '888 474', '24/12/18', 3, 950, 'Nice place', 'MediaCity UK MC55 1EX', 100);
