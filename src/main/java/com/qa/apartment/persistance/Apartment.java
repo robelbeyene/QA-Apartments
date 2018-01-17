5c53c0fa7f99377c7ee3e29e8bc8cce5d7cd8acf
Alasdair Stuart <AlasdairStuart@live.co.uk>
2018-01-17 12:03:05 +0000

    Changed apartment to work with date

--- src/main/java/com/qa/apartment/persistance/Apartment.java
+++ src/main/java/com/qa/apartment/persistance/Apartment.java
@@ -1,5 +1,7 @@
 package com.qa.apartment.persistance;
 
+import java.util.Date;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
@@ -7,9 +9,10 @@ import javax.validation.constraints.NotNull;
 public class Apartment {
 	
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
+	private Long id;
+	/*
 	@Column
-	@NotNull
+	@NotNull	
 	private String buildingName;
 	@Column
 	@NotNull
@@ -22,9 +25,12 @@ public class Apartment {
 	@Column
 	@NotNull
 	private String tenant;
+	*/
 	@Column
 	@NotNull
-	private String leaseStart;
+	@Temporal(TemporalType.DATE)
+	private Date leaseStart;
+	/*
 	@Column
 	@NotNull
 	private String leaseEnd;
@@ -35,10 +41,10 @@ public class Apartment {
 	@NotNull
 	private String agencyPhoneNo;
 	@Column
-	private int noRooms;
+	private Integer noRooms;
 	@Column
 	@NotNull
-	private double rent;
+	private Double rent;
 	@Column
 	private String notes;
 	@Column
@@ -46,115 +52,137 @@ public class Apartment {
 	private String address;
 	@Column
 	@NotNull
-	private double deposit;
+	private Double deposit;
+	*/
 	
 	public Apartment() {
 		
 	}
-	
-	public Apartment(String buildingName, String apartmentNo, String agency, String landlord, String tenant,
-			String leaseStart, String leaseEnd, String breakClause, String agencyPhoneNo, int noRooms, double rent,
-			String notes, String address, double deposit) {
-		super();
-		this.buildingName = buildingName;
-		this.apartmentNo = apartmentNo;
-		this.agency = agency;
-		this.landlord = landlord;
-		this.tenant = tenant;
-		this.leaseStart = leaseStart;
-		this.leaseEnd = leaseEnd;
-		this.breakClause = breakClause;
-		this.agencyPhoneNo = agencyPhoneNo;
-		this.noRooms = noRooms;
-		this.rent = rent;
-		this.notes = notes;
-		this.address = address;
-		this.deposit = deposit;
+
+	public Long getId() {
+		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/*
 	public String getBuildingName() {
 		return buildingName;
 	}
+
 	public void setBuildingName(String buildingName) {
 		this.buildingName = buildingName;
 	}
+
 	public String getApartmentNo() {
 		return apartmentNo;
 	}
+
 	public void setApartmentNo(String apartmentNo) {
 		this.apartmentNo = apartmentNo;
 	}
+
 	public String getAgency() {
 		return agency;
 	}
+
 	public void setAgency(String agency) {
 		this.agency = agency;
 	}
+
 	public String getLandlord() {
 		return landlord;
 	}
+
 	public void setLandlord(String landlord) {
 		this.landlord = landlord;
 	}
+
 	public String getTenant() {
 		return tenant;
 	}
+
 	public void setTenant(String tenant) {
 		this.tenant = tenant;
 	}
-	public String getLeaseStart() {
+	*/
+	public Date getLeaseStart() {
 		return leaseStart;
 	}
-	public void setLeaseStart(String leaseStart) {
+
+	public void setLeaseStart(Date leaseStart) {
 		this.leaseStart = leaseStart;
 	}
+	/*
 	public String getLeaseEnd() {
-		return this.leaseEnd;
+		return leaseEnd;
 	}
+
 	public void setLeaseEnd(String leaseEnd) {
 		this.leaseEnd = leaseEnd;
 	}
+
 	public String getBreakClause() {
 		return breakClause;
 	}
+
 	public void setBreakClause(String breakClause) {
 		this.breakClause = breakClause;
 	}
+
 	public String getAgencyPhoneNo() {
 		return agencyPhoneNo;
 	}
+
 	public void setAgencyPhoneNo(String agencyPhoneNo) {
 		this.agencyPhoneNo = agencyPhoneNo;
 	}
-	public int getNoRooms() {
+	
+	public Integer getNoRooms() {
 		return noRooms;
 	}
-	public void setNoRooms(int noRooms) {
+
+	public void setNoRooms(Integer noRooms) {
 		this.noRooms = noRooms;
 	}
-	public double getRent() {
+
+	public Double getRent() {
 		return rent;
 	}
-	public void setRent(double rent) {
+
+	public void setRent(Double rent) {
 		this.rent = rent;
 	}
+
 	public String getNotes() {
 		return notes;
 	}
+
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public double getDeposit() {
+
+	public Double getDeposit() {
 		return deposit;
 	}
-	public void setDeposit(double deposit) {
+
+	public void setDeposit(Double deposit) {
 		this.deposit = deposit;
-	}		
+	}
+	*/
+
+
+	
+	
 
 }
