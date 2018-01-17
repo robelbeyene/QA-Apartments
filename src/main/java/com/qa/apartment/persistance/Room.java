aae92c30ad167c6b94ec0fcd873d848af4ce4897
thisisgit1996 <leo.baker@qa.com>
2018-01-17 14:17:41 +0000

    Adding apartment attribute to room class.

--- src/main/java/com/qa/apartment/persistance/Room.java
+++ src/main/java/com/qa/apartment/persistance/Room.java
@@ -1,33 +1,44 @@
 package com.qa.apartment.persistance;
 
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Room {
 
-	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long roomId;
-	
-	
+
 	@ManyToOne
 	@JoinColumn(nullable = false, foreignKey = @ForeignKey(name = "fk_room_apartment"))
 	private Apartment apartment;
 
 	public Room() {
-		
+
 	}
-	
+
 	public Room(int roomId) {
 		super();
 		this.roomId = roomId;
 	}
-	
+
+	public Room(int roomId, Apartment apartment) {
+		super();
+		this.roomId = roomId;
+		this.apartment = apartment;
+	}
+
 	public long getRoomId() {
 		return roomId;
 	}
 
 	public void setRoomId(long roomId) {
 		this.roomId = roomId;
-	}	
+	}
 }
