ceb4c4ec579f6421301d8b11bdb7be6ef76591d6
chapmanoo <chapmanoo@hotmail.co.uk>
2018-01-17 12:54:09 +0000

    Tests for PersonService implemented

--- src/test/java/com/qa/apartment/business/PersonDBImpleTest.java
+++ src/test/java/com/qa/apartment/business/PersonDBImpleTest.java
@@ -19,26 +19,28 @@ public class PersonDBImpleTest {
 	private String testJsonStringB;
 	private Person aPerson;
 	private Person bPerson;
-	
+
 	@InjectMocks
-	PersonMapImpl impl;
+	PersonService impl;
 
 	@Mock
 	private JSONUtil util;
 
 	@Before
 	public void setUp() {
+
 		impl.setUtil(util);
-		
-		String testJsonStringA = "{\"first_name\": \"James\",\"last_name\": \"Herbert\",\"email\": \"test@test.com\", \"phone_number\": \"01234567891\"}";
-		String testJsonStringB = "{\"first_name\": \"Jenkins\",\"last_name\": \"Harlot\",\"email\": \"test2@test.com\", \"phone_number\": \"01234567891\"}";
-		
-		Person aPerson = new Person("James", "Herbert", "test@test.com", "01234567891");
-		Person bPerson = new Person("Jenkins", "Harlot", "test2@test.com", "01234567891");
+
+		testJsonStringA = "{\"first_name\": \"James\",\"last_name\": \"Herbert\",\"email\": \"test@test.com\", \"phone_number\": \"01234567891\"}";
+		testJsonStringB = "{\"first_name\": \"Jenkins\",\"last_name\": \"Harlot\",\"email\": \"test2@test.com\", \"phone_number\": \"01234567891\"}";
+
+		aPerson = new Person("James", "Herbert", "test@test.com", "01234567891");
+		bPerson = new Person("Jenkins", "Harlot", "test2@test.com", "01234567891");
 	}
 
 	@Test
 	public void testCreatePersonFromString() {
+
 		Mockito.when(util.getObjectForJSON(testJsonStringA, Person.class)).thenReturn(aPerson);
 
 		String returnedString = impl.createPersonFromString(testJsonStringA);
@@ -52,22 +54,69 @@ public class PersonDBImpleTest {
 		String returnedString = impl.createPersonFromPerson(aPerson);
 		Assert.assertEquals("{\"message\": \"person sucessfully added\"}", returnedString);
 	}
-	
+
 	@Test
-	public void updatePersonFromString() {
+	public void testUpdatePersonFromString() {
 		Mockito.when(util.getObjectForJSON(testJsonStringA, Person.class)).thenReturn(aPerson);
 		Mockito.when(util.getObjectForJSON(testJsonStringB, Person.class)).thenReturn(bPerson);
-		
+
 		String returnedString = impl.createPersonFromString(testJsonStringA);
 		Assert.assertEquals("{\"message\": \"person sucessfully added\"}", returnedString);
 
 		returnedString = impl.updatePersonFromString(1L, testJsonStringB);
 		Assert.assertEquals("{\"message\": \"person sucessfully updated\"}", returnedString);
 
-		returnedString = impl.updatePersonFromString(1L, testJsonStringB);
+		returnedString = impl.updatePersonFromString(5L, testJsonStringB);
 		Assert.assertEquals("{\"message\": \"person not updated see log\"}", returnedString);
 
-		
+	}
+
+	@Test
+	public void testUpdatePersonFromPerson() {
+		String returnedString = impl.createPersonFromPerson(aPerson);
+		Assert.assertEquals("{\"message\": \"person sucessfully added\"}", returnedString);
+
+		returnedString = impl.updatePersonFromPerson(1L, bPerson);
+		Assert.assertEquals("{\"message\": \"person sucessfully updated\"}", returnedString);
+
+		returnedString = impl.updatePersonFromPerson(5L, bPerson);
+		Assert.assertEquals("{\"message\": \"person not updated see log\"}", returnedString);
+
+	}
+
+	@Test
+	public void testDeletePerson() {
+		Mockito.when(util.getObjectForJSON(testJsonStringA, Person.class)).thenReturn(aPerson);
+
+		impl.createPersonFromString(testJsonStringA);
+
+		String returnedString = impl.deletePerson(1L);
+		Assert.assertEquals("{\"message\": \"person sucessfully deleted\"}", returnedString);
+
+		Assert.assertNull(impl.findPerson(1L));
+	}
+
+//	@Test
+//	public void testFindAllPersons() {
+//		Mockito.when(util.getObjectForJSON(testJsonStringA, Person.class)).thenReturn(aPerson);
+//		Mockito.when(util.getObjectForJSON(testJsonStringB, Person.class)).thenReturn(bPerson);
+//		
+//		impl.createPersonFromString(testJsonStringA);
+//		System.out.println(impl.findAllPersons());
+//		System.out.println(util.getJSONForObject(impl.getMap()));
+//
+//	} THERE IS A BUG WITH getJSONForObject which returns null
+
+	@Test
+	public void testFindPerson() {
+		Mockito.when(util.getObjectForJSON(testJsonStringA, Person.class)).thenReturn(aPerson);
+
+		Assert.assertNull(impl.findPerson(1L));
+
+		impl.createPersonFromString(testJsonStringA);
+
+		Assert.assertEquals(impl.findPerson(1L), aPerson);
+
 	}
 
 }
